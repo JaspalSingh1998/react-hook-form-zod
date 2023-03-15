f6f242b5969f2303854515b3154f95bbebb54789
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-03-15 19:21:32 -0400

    Integrated expense form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter"
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList"
+
+export const categories = ["Groceries", "Utilities", "Entertainment"]
 
 function App() {
   const [selectedFilter, setSelectedFilter] = useState('');
@@ -15,6 +18,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter onSelectCategory={(category) => setSelectedFilter(category)}/>
       </div>
