38b82719c5f48e19b2a7089520f7e6ca5109c777
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-03-15 17:18:33 -0400

    installed and imported zod for validations

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,9 @@
 import { useForm, FieldValues } from "react-hook-form";
+import {z} from 'zod';
 
 const Form = () => {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState} = useForm();
+    const {errors} = formState;
     const onSubmit = (data: FieldValues) => {
         console.log(data)
     }
@@ -9,8 +11,10 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input {...register('name')} type="text" className="form-control" id='name'/>
+            <input {...register('name', {required: true, minLength: 3})} type="text" className="form-control" id='name'/>
         </div>
+        {errors.name?.type === 'required' && <p>The Name Field is Required</p>}
+        {errors.name?.type === 'minLength' && <p>The Name must be at least 3  characters long</p>}
         <div className="mb-3">
             <label htmlFor="age" className="form-label">Age</label>
             <input {...register('age')} type="number" className="form-control" id='age'/>
